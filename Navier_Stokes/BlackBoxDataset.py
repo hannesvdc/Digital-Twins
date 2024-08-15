bb94c62826db7cb6cecb5ef0fffca751ef79a65a
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-08-15 16:40:27 -0400

    Some code cleanup in Navier-Stokes dataset.

--- Navier_Stokes/BlackBoxDataset.py
+++ Navier_Stokes/BlackBoxDataset.py
@@ -43,31 +43,15 @@ class NSDataSet(Dataset):
         dydxxx_data = dydxxx_data.flatten('F')
         dydxxxx_data = dydxxxx_data.flatten('F')
 
-        # TODO check order and rescale input and output data.
-        self.scale = 1.e4
-        self.y_scale = stats.gmean(np.abs(y_data))
-        self.dydt_scale = stats.gmean(np.abs(dydt_data))
-        self.dydx_scale = stats.gmean(np.abs(dydx_data))
-        self.dydxx_scale = stats.gmean(np.abs(dydxx_data))
-        self.dydxxx_scale = stats.gmean(np.abs(dydxxx_data))
-        self.dydxxxx_scale = stats.gmean(np.abs(dydxxxx_data))
-        print(np.mean(y_data), self.y_scale)
-        print(np.mean(dydx_data), self.dydx_scale)
-        print(np.mean(dydxx_data), self.dydxx_scale)
-        print(np.mean(dydxxx_data), self.dydxxx_scale)
-        print(np.mean(dydxxxx_data), self.dydxxxx_scale)
-        print(np.mean(dydt_data), self.dydt_scale)
-
         # Convert spatial derivatives to pytorch without gradients
-        pt.set_grad_enabled(False)
-        self.output_data = pt.unsqueeze(pt.from_numpy(dydt_data), dim=1) * self.scale #/ self.dydt_scale
-        self.input_data = pt.zeros((y_data.size, 5))
-        self.input_data[:,0] = pt.from_numpy(y_data) * self.scale #/ self.y_scale
-        self.input_data[:,1] = pt.from_numpy(dydx_data) * self.scale #/ self.dydx_scale
-        self.input_data[:,2] = pt.from_numpy(dydxx_data) * self.scale #/ self.dydxx_scale
-        self.input_data[:,3] = pt.from_numpy(dydxxx_data) * self.scale #/ self.dydxxx_scale
-        self.input_data[:,4] = pt.from_numpy(dydxxxx_data) * self.scale #/ self.dydxxxx_scale
-        pt.set_grad_enabled(True)
+        self.scale = 1.e4
+        self.input_data = pt.zeros((y_data.size, 5)).requires_grad_(False)
+        self.input_data[:,0] = self.scale * pt.from_numpy(y_data)
+        self.input_data[:,1] = self.scale * pt.from_numpy(dydx_data)
+        self.input_data[:,2] = self.scale * pt.from_numpy(dydxx_data)
+        self.input_data[:,3] = self.scale * pt.from_numpy(dydxxx_data)
+        self.input_data[:,4] = self.scale * pt.from_numpy(dydxxxx_data)
+        self.output_data = self.scale * pt.unsqueeze(pt.from_numpy(dydt_data), dim=1).requires_grad_(False)
 
     def __len__(self):
         return self.input_data.shape[0]

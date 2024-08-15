e6b0bc65cf2c7e1f5ccaa2e485af04adb125dce5
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-08-15 15:31:02 -0400

    Added loss gradient to weights in information print and Loss visualization.

--- Navier_Stokes/TrainBlackboxNetwork.py
+++ Navier_Stokes/TrainBlackboxNetwork.py
@@ -14,7 +14,7 @@ pt.set_default_dtype(pt.float64)
 
 # Load the data in memory
 print('Generating Training Data.')
-batch_size = 64
+batch_size = 300300
 dataset = NSDataSet()
 print(len(dataset))
 train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -27,11 +27,18 @@ optimizer = optim.Adam(network.parameters(), lr=0.001)
 
 # Training Routine
 train_losses = []
+train_grads = []
 train_counter = []
 store_directory = '/Users/hannesvdc/OneDrive - Johns Hopkins/Research_Data/Digital Twins/WaveData/'
+def computeGradNorm():
+    grads = []
+    for param in network.parameters():
+        grads.append(param.grad.view(-1))
+    grads = pt.cat(grads)
+    return pt.norm(grads)
 def train(epoch):
     network.train()
-    for batch_id, (input_data, output_data) in enumerate(train_loader):
+    for _, (input_data, output_data) in enumerate(train_loader):
         optimizer.zero_grad()
 
         # Foward-propagate the input data
@@ -42,20 +49,22 @@ def train(epoch):
 
         # Compute loss gradient
         loss.backward()
+        loss_grad = computeGradNorm()
 
         # Do one Adam optimization step
         optimizer.step()
 
     # Some housekeeping
-    print('Train Epoch: {} \tLoss: {:.16f}'.format(epoch, loss.item()))
+    print('Train Epoch: {} \tLoss: {:.16f} \tLoss Gradient {:.16f}'.format(epoch, loss.item(), loss_grad.item()))
     train_losses.append(loss.item())
+    train_grads.append(loss_grad.item())
     train_counter.append(epoch)
     pt.save(network.state_dict(), store_directory + 'model_black_box.pth')
     pt.save(optimizer.state_dict(), store_directory + 'optimizer_black_box.pth')
 
 # Do the actual training
 print('\nStarting Adam Training Procedure...')
-n_epochs = 1
+n_epochs = 10000
 try:
     for epoch in range(1, n_epochs+1):
         train(epoch)
@@ -63,8 +72,8 @@ except KeyboardInterrupt:
     print('Terminating Training. Plotting Training Error Convergence.')
 
 # Show the training results
+plt.semilogy(train_counter, train_grads, color='orange', label='Training Gradient')
 plt.semilogy(train_counter, train_losses, color='blue', label='Training Loss')
 plt.legend()
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
+plt.xlabel('Epoch')
 plt.show()

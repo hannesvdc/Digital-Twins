8e5c037bc06512109adb1110231d3103936b36aa
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-02 08:56:57 -0400

    Cleaning up a bit.

--- FitzHughNagumo/EulerTimestepper.py
+++ FitzHughNagumo/EulerTimestepper.py
@@ -122,18 +122,17 @@ def findSteadyState():
     eps = 0.1 # eps = 0.01 is unstable (oscillatory) beyond the Hopf bifurcation. Doing stable for now.
     params = {'delta': delta, 'eps': eps, 'a0': a0, 'a1': a1}
 
-    F = lambda x: psi(x, T, dx, dt, params)
     x_array = np.linspace(0.0, L, N)
     u0 = sigmoid(x_array, 14.0, -1, 1.0, 2.0)
     v0 = sigmoid(x_array, 15, 0.0, 2.0, 0.1)
     x0 = np.concatenate((u0, v0))
+    F = lambda x: psi(x, T, dx, dt, params)
     tolerance = 1.e-14
     try:
         cb = lambda x, f: print(lg.norm(f))
         x_ss = opt.newton_krylov(F, x0, rdiff=1.e-8, callback=cb, f_tol=tolerance)
     except opt.NoConvergence as err:
-        str_err = str(err)
-        str_err = str_err[1:len(str_err)-1]
+        str_err = str(err)[1:len(str_err)-1]
         x_ss = np.fromstring(str_err, dtype=float, sep=' ')
 
     u_ss = x_ss[0:N]

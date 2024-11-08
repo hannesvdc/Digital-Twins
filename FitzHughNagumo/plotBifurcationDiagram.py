ee946f4fa93903774a878daad271d22c40a4548c
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-08 15:59:47 -0500

    Implemented plotting code.

--- FitzHughNagumo/plotBifurcationDiagram.py
+++ FitzHughNagumo/plotBifurcationDiagram.py
@@ -12,37 +12,18 @@ dt = 0.001
 T = 1.0
 
 def plotBifurcationDiagram():
-    
+    M = 2 * N
+    # Load Continuation data, Arnoldi and QR data
+    directory = '/Users/hannesvdc/OneDrive - Johns Hopkins/Research_Data/Digital Twins/FitzhughNagumo/'
+    bf_data = np.load(directory + 'bf_diagram.npy')
+    arnoldi_values = np.load(directory + 'Arnoldi_Eigenvalues.npy')
+    qr_values = np.load(directory + 'QR_Eigenvalues.npy')
 
-    # Calculate eigenvalues on x1_path and x2_path
-    plot_x1_path = []
-    plot_x2_path = []
-    correct_eig1_vals = []
-    correct_eig2_vals = []
-    for i in range(len(x1_path)):
-        print('i = ', i)
-        plot_x1_path.append(np.mean(x1_path[i][0:N]))
-        plot_x2_path.append(np.mean(x2_path[i][0:N]))
-
-        if i % 10 != 0:
-            continue
-
-        A1 = slg.LinearOperator(shape=(M, M), matvec=lambda w: dGdx_v(x1_path[i], w, eps1_path[i]))
-        A1_matrix = np.zeros((M, M))
-        for k in range(M):
-            A1_matrix[:,k] = A1(np.eye(M)[:,k])
-        eig_vals1 = lg.eigvals(A1_matrix)
-        correct_eig1_vals.append(eig_vals1[np.argmin(np.real(eig_vals1))])
-
-        A2 = slg.LinearOperator(shape=(M, M), matvec=lambda w: dGdx_v(x2_path[i], w, eps2_path[i]))
-        A2_matrix = np.zeros((M, M))
-        for k in range(M):
-            A2_matrix[:,k] = A2(np.eye(M)[:,k])
-        eig_vals2 = np.sort(lg.eigvals(A2_matrix)) # Sort in ascending order
-        for j in range(len(eig_vals2)):
-            if np.imag(eig_vals2[j]) != 0.0:
-                correct_eig2_vals.append(eig_vals2[j])
-                break
+    n_steps = bf_data.shape[0]
+    plot_x1_path = np.average(bf_data[:,0:N], axis=1)
+    eps1_path = bf_data[:, M]
+    plot_x2_path = np.average(bf_data[:,M+1 : M+1+N], axis=1)
+    eps2_path = bf_data[:, -1]
 
     # Plot both branches
     plt.plot(eps1_path, plot_x1_path, color='blue', label='Branch 1')
@@ -51,16 +32,18 @@ def plotBifurcationDiagram():
     plt.ylabel(r'$<u>$')
 
     plt.figure()
-    plt.plot(np.linspace(0, max_steps, len(eig_vals1)), np.real(eig_vals1), color='blue', label='Branch 1')
-    plt.plot(np.linspace(0, max_steps, len(correct_eig1_vals)), np.real(correct_eig1_vals), color='black', label='Exact Eigenvalues')
+    plt.plot(np.linspace(0, n_steps-1, n_steps), np.real(arnoldi_values[0,:]), color='blue', label='Arnoldi Eigenvalues')
+    plt.plot(np.linspace(0, n_steps-1, n_steps), np.real(qr_values[0,:]), color='black', label='QR Eigenvalues (Exact)')
+    plt.title('Branch 1')
     plt.xlabel('Continuation Step')
     plt.ylabel('Eigenvalue')
     plt.legend()
     
     plt.figure()
-    plt.scatter(np.real(eig_vals2), np.abs(np.imag(eig_vals2)), color='red', label='Branch 2')
-    plt.scatter(np.real(correct_eig2_vals), np.abs(np.imag(correct_eig2_vals)), color='black', label='Exact Eigenvalues')
-    plt.ylabel('Imaginary') 
+    plt.scatter(np.real(arnoldi_values[1,:]), np.abs(np.imag(arnoldi_values)[1,:]), color='red', label='Arnoldi Eigenvalues')
+    plt.scatter(np.real(qr_values[1,:]), np.abs(np.imag(qr_values[1,:])), color='black', label='QR Eigenvalues (Exact)')
+    plt.title('Branch 2')
+    plt.ylabel('Imaginary')
     plt.xlabel('Real')
     plt.legend()
     plt.show()

24355ef979b724742988d001404c117253064bc2
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-15 11:50:46 -0500

    Tested clamped spline interpolation.

--- FitzHughNagumo/BSpline.py
+++ FitzHughNagumo/BSpline.py
@@ -3,21 +3,20 @@ import numpy.linalg as lg
 
 class ClampedCubicSpline:
     def __init__(self, x, f, x_left=0.0, x_right=1.0):
-        super(ClampedCubicSpline).__init__(self)
-
-        self.x = x
-        self.f = f
+        self.x = np.copy(x)
+        self.f = np.copy(f)
         self.n = len(self.x) - 1
+        self.x[-1] = self.x[-1] + 1.e-12
 
         A = np.zeros((4 * self.n, 4 * self.n))
 
         # interpolation conditions
         for i in range(self.n):
-            A[2*i,0] = 1.0
-            A[2*i+1, 0] = 1.0
-            A[2*i+1, self.n] = self.x[i+1] - self.x[i]
-            A[2*i+1, 2*self.n] = (self.x[i+1] - self.x[i])**2
-            A[2*i+1, 3*self.n] = (self.x[i+1] - self.x[i])**3
+            A[2*i,i] = 1.0 # Left point
+            A[2*i+1, i] = 1.0 # Right point ->
+            A[2*i+1, self.n + i] = self.x[i+1] - self.x[i]
+            A[2*i+1, 2*self.n + i] = (self.x[i+1] - self.x[i])**2
+            A[2*i+1, 3*self.n + i] = (self.x[i+1] - self.x[i])**3
 
         # Derivative continuity conditions
         for i in range(self.n-1):
@@ -28,17 +27,17 @@ class ClampedCubicSpline:
 
         # Second derivative continuity conditions
         for i in range(self.n-1):
-            A[3*self.n+i, 2*self.n + i] = 2.0
-            A[3*self.n+i, 3*self.n + i] = 6.0 * (self.x[i+1] - self.x[i])
-            A[3*self.n+i, 2*self.n + i + 1] = -2.0
+            A[3*self.n+i-1, 2*self.n + i] = 2.0
+            A[3*self.n+i-1, 3*self.n + i] = 6.0 * (self.x[i+1] - self.x[i])
+            A[3*self.n+i-1, 2*self.n + i + 1] = -2.0
 
         # Clamped boundary conditions
         A[-2, self.n] = 1.0
         A[-2, 2*self.n] = 2.0 * (x_left - self.x[0])
         A[-2, 3*self.n] = 3.0 * (x_left - self.x[0])**2
         A[-1, 2*self.n-1] = 1.0
-        A[-1, 3*self.n-1] = 2.0 * (x_right - self.x[self.n])
-        A[-1, 4*self.n-1] = 3.0 * (x_right - self.x[self.n])**2
+        A[-1, 3*self.n-1] = 2.0 * (x_right - self.x[self.n-1])
+        A[-1, 4*self.n-1] = 3.0 * (x_right - self.x[self.n-1])**2
 
         # Right-hand side
         b = np.zeros(4 * self.n)
@@ -59,10 +58,11 @@ class ClampedCubicSpline:
         return self.evaluate(x)
     
     def evaluate(self, x):
-        index = np.searchsorted(self.x, x, side='left')
+        index = np.searchsorted(self.x, x, side='right') - 1
+        print('index', index, x, self.x[index])
         return self.a[index] + self.b[index] * (x - self.x[index]) + self.c[index] * (x - self.x[index])**2 + self.d[index] * (x - self.x[index])**3
     
     def derivative(self, x):
-        index = np.searchsorted(self.x, x, side='left')
+        index = np.searchsorted(self.x, x, side='right') - 1
         return self.b[index] + 2.0 * self.c[index] * (x - self.x[index]) + 3.0 * self.d[index] * (x - self.x[index])**2
 
---
+++ FitzHughNagumo/tests/testSpline.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import BSpline
+
+# An approximate bell function with mean 0.5 and variance 0.1
+def test1():
+    mu = 0.5
+    sigma_sq = 0.1
+    x_values = np.linspace(0.0, 1.0, 20)
+    f_values = 1.0 / np.sqrt(2.0*np.pi*sigma_sq) * np.exp(-(x_values - mu)**2 / (2.0*sigma_sq))
+    spline = BSpline.ClampedCubicSpline(x_values, f_values)
+
+    plot_x_values = np.linspace(0.0, 1.0, 1000)
+    spline_values = np.zeros_like(plot_x_values)
+    for i in range(len(plot_x_values)):
+        spline_values[i] = spline(plot_x_values[i])
+
+    plt.plot(x_values, f_values+0.01, label='Original function')
+    plt.plot(plot_x_values, spline_values, label='Spline Interpolation')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    test1()

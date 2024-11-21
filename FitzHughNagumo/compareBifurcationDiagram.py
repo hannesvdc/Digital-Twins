edfe235af43a8e3649fc3ba7f921b37cc46cd5b6
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-21 18:04:02 -0500

    Added code to calculate location of the Hopf bifurcation.

--- FitzHughNagumo/compareBifurcationDiagram.py
+++ FitzHughNagumo/compareBifurcationDiagram.py
@@ -9,24 +9,34 @@ directory = '/Users/hannesvdc/OneDrive - Johns Hopkins/Research_Data/Digital Twi
 euler_bf_file = 'bf_diagram.npy'
 gaptooth_bf_file = 'toothnogap_bf_diagram.npy'
 
+is_sorted = lambda a: np.all(a[:-1] <= a[1:])
+
 # Plot The Euler diagram first
 N = 200
+M = 2*N
 euler_data = np.load(directory + euler_bf_file)
-euler_p1 = euler_data[:,0:2*N]
-euler_eps1 = euler_data[:,2*N]
-euler_p2 = euler_data[:,2*N+1:4*N+1]
-euler_eps2 = euler_data[:,4*N+1]
-
+euler_p1 = euler_data[:,0:M]
+euler_eps1 = euler_data[:,M]
+euler_p2 = euler_data[:,M+1:2*M+1]
+euler_eps2 = euler_data[:,2*M+1]
 euler_u_data_p1 = np.average(euler_p1[:,0:N], axis=1)
 euler_u_data_p2 = np.average(euler_p2[:,0:N], axis=1)
-plt.plot(euler_eps1, euler_u_data_p1, color='blue', label='Euler Timestepper')
-plt.plot(euler_eps2, euler_u_data_p2, color='blue')
+
+# Find the location of the Hopf Bifurcation
+eps_hopf = 0.015
+p = euler_eps2.argsort()
+euuler_eps2_sorted = euler_eps2[p]
+euler_u_data_p2_sorted = euler_u_data_p2[p]
+hopf_index = np.searchsorted(euuler_eps2_sorted, eps_hopf)
+u_hopf = euler_u_data_p2_sorted[hopf_index]
+
+plt.plot(euler_eps1, euler_u_data_p1, color='tab:blue', label='Euler Timestepper')
+plt.plot(euler_eps2, euler_u_data_p2, color='tab:blue')
 
 # Then plot the Tooth-Without-Gaps diagram (substitute for gap-tooth bf diagram for now)
 gt_data = np.load(directory + gaptooth_bf_file)
 M = (gt_data.shape[1] - 2) // 2
 N = M // 2
-print(gt_data.shape, M, N)
 gt_p1 = gt_data[:,0:M]
 gt_eps1 = gt_data[:,M]
 gt_p2 = gt_data[:,M+1:2*M+1]
@@ -34,10 +44,12 @@ gt_eps2 = gt_data[:,2*M+1]
 
 gt_u_data_p1 = np.average(gt_p1[:,0:N], axis=1)
 gt_u_data_p2 = np.average(gt_p2[:,0:N], axis=1)
-plt.plot(euler_eps1, euler_u_data_p1+0.002, color='orange', label='Gap-Tooth Timestepper')
-plt.plot(euler_eps2, euler_u_data_p2+0.002, color='orange')
+plt.plot(gt_eps1, gt_u_data_p1, color='tab:orange', label='Gap-Tooth Timestepper')
+plt.plot(gt_eps2, gt_u_data_p2, color='tab:orange')
 plt.xlabel(r'$\varepsilon$')
 plt.ylabel(r'$<u>$', rotation=0)
+
+plt.scatter([eps_hopf], [u_hopf], color='red', label='Hopf Bifurcation')
 
 plt.legend()
 plt.show()

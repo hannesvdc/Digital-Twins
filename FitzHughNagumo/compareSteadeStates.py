9c6cec034450392dc83d439b3c031a652bd17c41
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-23 13:22:47 -0500

    Small bugfix.

--- FitzHughNagumo/compareSteadeStates.py
+++ FitzHughNagumo/compareSteadeStates.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ToothNoGapTimestepper import findSteadyStateNewtonGMRES as calc_ss_tooth
 from GapToothTimestepper import findSteadyStateNewtonGMRES as calc_ss_gaptooth
 
 directory = '/Users/hannesvdc/OneDrive - Johns Hopkins/Research_Data/Digital Twins/FitzhughNagumo/'
@@ -13,15 +12,13 @@ U_euler = euler_data[1,:]
 V_euler = euler_data[2,:]
 
 print('\nCalculating Steady-State of the Tooth-No-Gap Scheme')
-x_tooth, ss_tooth = calc_ss_tooth(return_ss=True)
+gap_no_tooth_data = np.load(directory + 'gap_no_tooth_steady_state.npy')
+x_tooth = gap_no_tooth_data[0,:]
+U_tooth = gap_no_tooth_data[1,:]
+V_tooth = gap_no_tooth_data[2,:]
 
 print('\nCalculating Steady-State of the Gap-Tooth Scheme')
 x_gaptooth, U_ss_gaptooth, V_ss_gaptooth = calc_ss_gaptooth(return_ss=True)
-
-# Extract U and V for euler and the tooth-scheme
-x_tooth = np.concatenate(x_tooth)
-N_tooth = len(ss_tooth) // 2
-U_tooth, V_tooth = ss_tooth[0:N_tooth], ss_tooth[N_tooth:]
 
 for i in range(len(x_gaptooth)):
     if i == 0:
@@ -32,8 +29,8 @@ for i in range(len(x_gaptooth)):
         plt.plot(x_gaptooth[i], V_ss_gaptooth[i], color='tab:orange')
 plt.plot(x_euler, U_euler, label=r'Euler $u(x)$', color='tab:green')
 plt.plot(x_euler, V_euler, label=r'Euler $v(x)$', color='tab:red')
-#plt.plot(x_tooth, U_tooth, label=r'Tooth-No-Gap $u(x)$', color='tab:purple')
-#plt.plot(x_tooth, V_tooth, label=r'Tooth-No-Gap $v(x)$', color='tab:brown')
+plt.plot(x_tooth, U_tooth, label=r'Tooth-No-Gap $u(x)$', color='tab:purple')
+plt.plot(x_tooth, V_tooth, label=r'Tooth-No-Gap $v(x)$', color='tab:brown')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$u, v$', rotation=0)
 plt.title('Newton-Krylov Steady-State')

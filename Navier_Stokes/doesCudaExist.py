327f0ef1e1f2a9c646612c8c21fddebe4baa9c4a
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-02-03 16:26:17 -0500

    Removing cuda test script

--- Navier_Stokes/doesCudaExist.py
+++
@@ -1,9 +0,0 @@
-import torch as pt
-
-if pt.cuda.is_available():
-    print("CUDA is available")
-    print("Number of devices: ", pt.cuda.device_count())
-    device = pt.cuda.current_device()
-    print('Device name:', pt.cuda.get_device_name(device))
-else:
-    print("CUDA is not available")
